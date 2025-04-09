a3ef3ff88e8788a182fb1032816d4b22eb5ecebe
nataligvozd04 <nataligvozd04@gmail.com>
2025-04-09 20:45:38 +0500

    fix: add alt for images

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ function createCard(name, link, deleteCard) {
 
     cardElement.querySelector('.card__title').textContent = name;
     cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
     
     const deleteButton = cardElement.querySelector('.card__delete-button')
     deleteButton.addEventListener('click', () => {
